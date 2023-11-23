ed449ce207580e50caf7c58ef4fa631f0ce2f9fe
Smit Koli <koli.smit.18ce5005@gmail.com>
2023-11-23 16:36:51 +0530

    Pushing all changes

---
+++ chatbot-ui/src/App.vue
@@ -0,0 +1,21 @@
+<!-- App.vue -->
+<template>
+  <div id="app">
+    <ChatBot />
+  </div>
+</template>
+
+<script>
+import ChatBot from '@/components/ChatBot.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ChatBot,
+  },
+};
+</script>
+
+<style>
+/* Add your global styles here if needed */
+</style>
